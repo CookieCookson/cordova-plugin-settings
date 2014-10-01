35602c1832ee1ed9c218d18a41a415ee2c96366a
Christian Cook <christian@elixel.co.uk>
2014-10-01 12:43:22 +0100

    removed legacy device code

--- src/ios/CDVSettings.m
+++ src/ios/CDVSettings.m
@@ -8,16 +8,11 @@
 
 - (void)openSettings:(CDVInvokedUrlCommand*)command
 {
-    NSDictionary* deviceProperties = [self deviceProperties];
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:deviceProperties];
-
     BOOL canOpenSettings = (&UIApplicationOpenSettingsURLString != NULL);
     if (canOpenSettings) {
         NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
         [[UIApplication sharedApplication] openURL:url];
     }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
